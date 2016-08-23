e332fb841941758adaa4ad19bd230c39c861f709
wilqor <wilqor@gmail.com>
2016-08-23 21:50:39 +0200

    Add ComponentScan annotation to get rid of Service injection errors in inspection

--- WLObs-server/src/main/java/com/wlobs/wilqor/server/Server.java
+++ WLObs-server/src/main/java/com/wlobs/wilqor/server/Server.java
@@ -18,11 +18,13 @@ package com.wlobs.wilqor.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author wilqor
  */
 @SpringBootApplication
+@ComponentScan
 public class Server {
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
