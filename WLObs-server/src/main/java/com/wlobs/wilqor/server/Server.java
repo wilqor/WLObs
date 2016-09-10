39ce17a451568d45634cfb247091c212b2372593
wilqor <wilqor@gmail.com>
2016-09-10 15:44:07 +0200

    Allow also non-HTTPS connection on another port

--- WLObs-server/src/main/java/com/wlobs/wilqor/server/Server.java
+++ WLObs-server/src/main/java/com/wlobs/wilqor/server/Server.java
@@ -16,8 +16,12 @@
 
 package com.wlobs.wilqor.server;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -26,6 +30,19 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 public class Server {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
     }
